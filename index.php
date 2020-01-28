40f43ec9f106b67dfb6ebdf2133d98ea02b83cc4
Lucas da Cunha Victor <lucas.victor@totvs.com.br>
2020-01-28 14:44:22 -0300

    Correções para alinhamento com o Marketing

--- index.php
+++ index.php
@@ -2,9 +2,32 @@
 <html>
     <head>
         <title>Eleve Gestão</title>
-        <link href="css/style.css" rel="stylesheet" type="text/css">
+            <link href="css/style.css" rel="stylesheet" type="text/css">
+            <!-- Compiled and minified CSS -->
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+            <!-- Compiled and minified JavaScript -->
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
     </head>
     <body class="fundo" >
-        
+        <div class="row">
+            <div class="col s12 m6 l6 offset-l1 offset-m2 eleve">
+                <h3>Conheça o Eleve Gestão <br> e tenha o controle financeiro <br> total da sua empresa</h3>
+                <h5>
+                    <ul>
+                        <li> <img src="img/check_box.png" class="box_check"> Emita notas e organize ordens de serviço</li>
+                        <li> <img src="img/check_box.png" class="box_check"> Controle o seu fluxo de caixa com facilidade</li>
+                        <li> <img src="img/check_box.png" class="box_check"> Controle compras realizadas e estoque </li>
+                    </ul>
+                </h5>
+                <h5> Escolha o Eleve, a melhor opção em sistema <br> para controle financeiro e gestão de estoque </h5>
+                <br>
+                <h3 >
+                    <a class="btn eleve-btn" href="https://elevesuasvendas.com.br" target="_balnk">Saiba Mais</a>
+                <h3>
+            </div>
+        </div>
     </body>
 </html>
