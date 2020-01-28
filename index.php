7b4e7617b7d87d16c3a17b239f1fc9c90a84d035
Lucas da Cunha Victor <lucas.victor@totvs.com.br>
2020-01-28 11:04:16 -0300

    Inicialização

---
+++ index.php
@@ -0,0 +1,9 @@
+
+<html>
+    <head>
+        <title>Eleve Gestão</title>
+    </head>
+    <body src="https://cdnmpn.blob.core.windows.net/suporte/ELEVE-BANNER.jpg">
+        
+    </body>
+</html>
