d5e25e453567c5fdc89b81c55235ca256219d443
Lucas da Cunha Victor <lucas.victor@totvs.com.br>
2020-01-28 11:08:24 -0300

    troca de src

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <title>Eleve Gestão</title>
     </head>
-    <body src="https://cdnmpn.blob.core.windows.net/suporte/ELEVE-BANNER.jpg">
+    <body background="https://cdnmpn.blob.core.windows.net/suporte/ELEVE-BANNER.jpg">
         
     </body>
 </html>
