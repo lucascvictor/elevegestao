047be8a6a1903e7d2bbe97e7bdb511ed13471ec2
Lucas da Cunha Victor <lucas.victor@totvs.com.br>
2020-01-28 11:12:42 -0300

    redimensionar fundo

--- index.php
+++ index.php
@@ -2,8 +2,14 @@
 <html>
     <head>
         <title>Eleve Gestão</title>
+        <style>
+            .fundo {
+                background: "https://cdnmpn.blob.core.windows.net/suporte/ELEVE-BANNER.jpg";
+                background-size: cover;
+            }
+        </style>
     </head>
-    <body background="https://cdnmpn.blob.core.windows.net/suporte/ELEVE-BANNER.jpg">
+    <body class="fundo" >
         
     </body>
 </html>
