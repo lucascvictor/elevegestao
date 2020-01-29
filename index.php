73a645a9b7a18269e008000f0b2fcdf4f4311e3f
Lucas da Cunha Victor <lucas.victor@totvs.com.br>
2020-01-29 11:28:46 -0300

    Correção de tamanhos

--- index.php
+++ index.php
@@ -14,19 +14,18 @@
     <body class="fundo" >
         <div class="row">
             <div class="col s12 m6 l6 offset-l1 offset-m2 eleve">
-                <img src="https://cdnmpn.blob.core.windows.net/img/logo/eleve_white.png" style="width: 170px; height: 100px; margin-bottom: 45px;">
+                <img src="https://cdnmpn.blob.core.windows.net/img/logo/eleve_white.png" style="width: 170px; height: 100px; margin-bottom: 40px;">
 
                 <h3>Conheça o Eleve Gestão <br> e tenha o controle financeiro <br> total da sua empresa</h3>
-                <h5>
+                <h6>
                     <ul>
                         <li> <img src="img/check_box.png" class="box_check"> Emita notas e organize ordens de serviço</li>
                         <li> <img src="img/check_box.png" class="box_check"> Controle o seu fluxo de caixa com facilidade</li>
                         <li> <img src="img/check_box.png" class="box_check"> Controle compras realizadas e estoque </li>
                     </ul>
-                </h5>
+                </h6>
                 <h5> Escolha o Eleve, a melhor opção em sistema <br> para controle financeiro e gestão de estoque </h5>
-
-                <h3 >
+                <h3>
                     <a class="btn eleve-btn" href="https://elevesuasvendas.com.br" target="_balnk">Saiba Mais</a>
                 <h3>
             </div>
